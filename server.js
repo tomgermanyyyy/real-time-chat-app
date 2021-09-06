10ce79e8bbe8e98ce97d120bef9d3c7b5a412c29
Viet Duc Nguyen <duc.nv291@gmail.com>
2021-09-06 16:43:34 +0700

    Prepare for deploying

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const path = require('path');
+const cors = require('cors');
 const router = require('./router');
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
@@ -9,6 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+app.use(cors());
 app.use(router);
 
 io.on('connection', socket => {
@@ -59,6 +62,13 @@ io.on('connection', socket => {
     }
   });
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
